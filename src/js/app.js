aa9e274afd785a6dbd16858cca55ad619400eaed
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-11-20 11:02:30 +0100

    Header (nav menu) + Home - Responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,39 @@
+/*jshint esversion: 6 */
+
 //HEADER BACKGROUND ON SCROLL
 $(window).scroll(() => {
     var scroll = $(window).scrollTop();
-    if(scroll > 200){
-        $('header').addClass('scrolled');
-    } else {
-        $('header').removeClass('scrolled');
+    var windowHeight = window.innerHeight;
+    if(windowHeight > 850){
+        if(scroll > 200){
+            $('header').addClass('scrolled');
+        } else {
+            $('header').removeClass('scrolled');
+        }
+    } else{
+        if(scroll > 100){
+            $('header').addClass('scrolled');
+        } else {
+            $('header').removeClass('scrolled');
+        }
+    }
+    
+});
+
+//NAV MENU
+const nav = $('nav');
+$('.open-menu').click(() => { //OUVERTURE
+    nav.css("display", "flex").hide().fadeIn();
+    nav.addClass('active');
+});
+$('.close-menu').click(() => { //FERMETURE
+    nav.removeClass('active');
+    nav.fadeOut();
+    
+});
+$(window).resize(() => { //REDIMENSIONNEMENT
+    if(window.innerWidth > 1200){
+        nav.removeClass('active');
+        nav.css({"display":""});
     }
 });
