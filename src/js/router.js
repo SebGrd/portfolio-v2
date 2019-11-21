adaf7f92dea75abfd6fdddc36758b6bfd0a586ad
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-11-21 16:03:38 +0100

    Mise à jour du router et création des views

--- src/js/router.js
+++ src/js/router.js
@@ -1,23 +1,43 @@
 /*jshint esversion: 6 */
 
-const path = window.location.pathname;
-const routes = {
-    '/': 'src/view/home.html',
-    '/centre-formation': 'src/view/ecole.html',
-    '/entreprise': 'src/view/entreprise.html',
-    '/projets': 'src/view/projets.html',
-    '/competences': 'src/view/competences.html',
-    '/veille': 'src/view/veille.html',
-    '/contact': 'src/view/contact.html'
-}
-
-function getPath(path, routes){
-    if(routes.path.length > 0){
-        
-    }
-}
 const request = new XMLHttpRequest();
-request.open("GET", "src/view/home.html", false);
+const path = window.location.pathname;
+const routes = { //LES ROUTES : URL(camel) : VIEW SRC
+    'home': 'src/view/home.html',
+    '404': 'src/view/404.html',
+    'centreDeFormation': 'src/view/ecole.html',
+    'entreprise': 'src/view/entreprise.html',
+    'projets': 'src/view/projets.html',
+    'competences': 'src/view/competences.html',
+    'veille': 'src/view/veille.html',
+    'contact': 'src/view/contact.html'
+};
+
+const toCamel = (s) => { //FONCTION KEBAB > CAMEL
+    return s.replace(/([-_][a-z])/ig, ($1) => {
+        return $1.toUpperCase()
+            .replace('-', '')
+            .replace('_', '');
+    });
+};
+
+const getPath = (routes) => { // RETOURNE LA SRC DE LA VUE DE LA PAGE
+    var path = window.location.pathname;
+    if (path === '/') {
+        return routes.home;
+    } else {
+        var cleanPath = path.substr(1);
+        var camelPath = toCamel(cleanPath);
+        if (routes[camelPath] !== undefined){
+            return routes[camelPath];
+        } else{
+            return routes['404'];
+        }
+
+    }
+};
+
+
+request.open("GET", getPath(routes), false);
 request.send();
-console.log(request);
 document.getElementById("page").innerHTML = request.response;
