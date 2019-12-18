ec16a95c1e172daf3055466aa055f3df105871ec
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-12-19 00:08:27 +0100

    Page compétences

--- src/view/competences.php
+++ src/view/competences.php
@@ -1,1 +1,76 @@
-<h2>Compétences</h2>
+<div class="page-header">
+    <div class="page-header_image">
+        <img src="src/img/bg-screen.png" alt="utec" class="img-resp center">
+    </div>
+    <div class="container">
+        <h2>Compétences</h2>
+    </div>
+</div>
+
+<section class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 order-0">
+                <h2>Langages</h2>
+                <ul class="divided-list">
+                    <li>HTML</li>
+                    <li>PHP</li>
+                    <li>CSS/SCSS</li>
+                    <li>Java</li>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                </ul>
+            </div>
+            <div class="col-md-5 order-1">
+                <img src="src/img/web-language.png" alt="illustration" class="img-resp">
+            </div>
+        </div>
+        <hr class="my-5">
+        <div class="row">
+            <div class="col-md-4 order-1">
+                <img src="src/img/framework.png" alt="illustration" class="img-resp">
+            </div>
+            <div class="col-md-8 order-0">
+                <h2>Framework</h2>
+                <ul class="divided-list">
+                    <li>Bootstrap</li>
+                    <li>Jquery</li>
+                    <li>Materialize</li>
+                    <li>VueJS</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-5">
+        <div class="row">
+            <div class="col-md-7 order-0">
+                <h2>Logiciels</h2>
+                <ul class="divided-list">
+                    <li>PhpStorm</li>
+                    <li>Photoshop</li>
+                    <li>Eclipse</li>
+                    <li>Figma</li>
+                    <li>GitKraken</li>
+                    <li>AdobeXD</li>
+                    <li>Visual Studio Code</li>
+                </ul>
+            </div>
+            <div class="col-md-5 order-1">
+                <img src="src/img/software.png" alt="illustration" class="img-resp">
+            </div>
+        </div>
+        <hr class="my-5">
+        <div class="row">
+            <div class="col-md-4 order-1">
+                <img src="src/img/gestion.png" alt="illustration" class="img-resp">
+            </div>
+            <div class="col-md-8">
+                <h2>Gestion de projets</h2>
+                <ul class="divided-list">
+                    <li>GitHub</li>
+                    <li>GitKraken</li>
+                    <li>GitLab</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
